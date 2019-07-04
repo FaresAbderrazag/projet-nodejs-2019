9e5b14eb3d1b00500b52c73b0e99a959f4bc8a7b
Abderrazag FARES <farse.abderrazag@gmail.com>
2019-07-04 10:46:44 +0200

    add module users

--- src/modules/users/services/createOne.js
+++ src/modules/users/services/createOne.js
@@ -1,20 +1,12 @@
 const { createModel } = require('../model');
 const connect = require('../../../clients/mongodb');
 const collections = require('../../../enums/collections');
-const listFindOneById = require('../../lists/services/findOneById');
 
-module.exports = (taskToCreate, listId) => {
-  const task = {
-    ...taskToCreate,
-    listId,
-    checked: false,
-  };
-
-  return createModel.validate(task)
-    .then(() => listFindOneById(listId))
-    .then(() => connect())
-    .then(db => db.collection(collections.TASKS))
-    .then(collection => collection.insertOne(task))
+module.exports = (userToCreate) => {
+  return createModel.validate(userToCreate)
+    .then(connect)
+    .then(db => db.collection(collections.USERS))
+    .then(collection => collection.insertOne(userToCreate))
     .then(dbResponse => dbResponse.ops[0]);
 
   /* Can also be write like the
